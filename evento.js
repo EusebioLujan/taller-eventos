ef0f9cd71d56f1494a99584570a5424b083e7fca
Marcelo Lujan <115477257+EusebioLujan@users.noreply.github.com>
2024-08-10 22:07:27 -0300

    fix click

--- evento.js
+++ evento.js
@@ -1,4 +1,10 @@
 const div = document.getElementById("div");
+const boton = document.getElementById("boton");
+
+boton.addEventListener("click",(event)=>{
+        event.stopPropagation(); // detiene la propagacion del evento para que frene
+        alert('hola');
+})
 div.addEventListener('click',()=>{
-    alert("Hola! Soy el div")
+    alert("Hola! Soy el div");
 })
